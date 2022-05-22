3e65fdbe9b84cd9b86254ce523788f4f2755f49f
scarneiromarcelo <s.carneiro.marcelo@gmail.com>
2022-05-22 19:01:00 -0300

    five commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,21 @@
-// const btn = document.querySelector('button');
-// const txt = document.getElementById('txt');
-// const img = document.getElementById('img');
-
-// const array = [
-//     ["Agora seria uma ótima hora para ficar com raiva Doutor!", "./assets/img/brucer.png"],
-//     ["Esse é o meu segredo, estou sempre com raiva !", "./assets/img/hulk.png"],
-//     ["Agora seria uma ótima hora para ficar com raiva Doutor!", "./assets/img/brucer.png"],
-// ];
-
-
-// btn.addEventListener('click', () => {
-    
-//     txt.innerText = `${array[1][0]}` 
-//     img.src = `${array[1][1]}`
-
-// });
-
-// let cont = 0
-// let img = document.querySelector("#imagem");
-// const mudaImg = () => {
-//     if (cont == 0){
-//         img.setAttribute('assets', './img/hulk.png');
-//         cont += 1
-//     } else if (cont == 1){
-//         img.setAttribute('assets', './img/brucer.png');
-//         cont -= 1
-//     }
-// }
-
-// var int = 2;
-// var img = document.getElementById("img");
-// function trocaImg(int){
-//   if (int == 1)
-//     {
-//       img.src = "./assets/img/brucer.png";
-//     }
-//   else if (int == 2)
-//     {
-//       img.src = "./assets/img/hulk.png";
-//     }
-// }
-
-var cont = 0
+var cont = 1;
 var img = document.querySelector("#image");
 
 const mudaimage = () => {
-
   if (cont == 0) {
-    img.setAttribute('assets', '/img/hulk.png');
-    document.getElementById('txt').innerText = 'Agora seria uma ótima hora para ficar com raiva Doutor!' 
- 
-    cont += 1
+    imagem.setAttribute("src", "./assets/img/brucer.png");
+    document.getElementById("txt").innerText =
+      "Agora seria uma ótima hora para ficar com raiva Doutor!";
+
+    cont += 1;
+  } else if (cont == 1) {
+    imagem.setAttribute("src", "./assets/img/hulk.png");
+    document.getElementById("txt").innerText =
+      "Esse é o meu segredo, estou sempre com raiva !";
+    cont += 1;
+  } else if (cont == 2) {
+    imagem.setAttribute("src", "./assets/img/shehulk.png");
+    document.getElementById("txt").innerText = "She Hulk !";
+    cont -= 2;
   }
-}
-
-
-
-
-
-
-
+};
